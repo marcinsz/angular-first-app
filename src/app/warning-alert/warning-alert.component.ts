0194d31d6be78922ae6b3906231871abbf27a1a5
Marcin Szot <marcin@Marcins-MacBook-Pro-2.local>
2018-01-09 00:16:37 +0100

    Warning component

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html'
+  styleUrl: './warning-alert.css'
+})
+export class WarningAlertComponent {
+  
+}
